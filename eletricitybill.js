907dfaaa203d4c19c2429d9ab1a13a7ed999a14c
ilokeshrao <143261634+ilokeshrao@users.noreply.github.com>
2025-06-30 22:05:35 +0530

    Add files via upload

---
+++ eletricitybill.js
@@ -0,0 +1,19 @@
+let unit = Number(prompt("enter your eletricity bill")); //700
+
+let amount = 0;
+if (unit > 400) {
+  amount = (unit - 400) * 13; //
+  unit = 400;
+} //400
+if (unit > 200 && unit <= 400) {
+  amount += (unit - 200) * 8;
+  unit = 200;
+} //200
+if (unit > 100 && unit <= 200) {
+  amount += (unit - 100) * 6;
+  unit = 100;
+}//100
+
+amount += unit * 4;
+
+console.log(amount);
